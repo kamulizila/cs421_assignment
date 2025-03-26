3a67a14c17c58832b8f2cd34bc51e691ba06b346
unknown <kamulizila97@gmail.com>
2025-03-26 19:31:31 +0300

    Initial commit

--- university/urls.py
+++ university/urls.py
@@ -3,8 +3,8 @@ from .views import student_list, subject_list
 from . import views
 
 urlpatterns = [
-    path('students/', student_list, name='student-list'),
-    path('subjects/', subject_list, name='subject-list'),
+    path('api/students/', student_list, name='student-list'),
+    path('api/subjects/', subject_list, name='subject-list'),
      path('', views.home, name='home'),  # Root URL pattern
     
 ]
